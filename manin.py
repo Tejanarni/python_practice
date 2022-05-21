6a5d7fc68703264da56e61144b181ff80e411bba
Teja Narni <75619472+Tejanarni@users.noreply.github.com>
2022-05-21 19:45:18 +0530

    Update manin.py

--- manin.py
+++ manin.py
@@ -14,3 +14,18 @@ for i in range(5):
     for j in range(5):
         print("*",end="")
     print("")
+
+
+for i in range(10):
+    if i==7:
+        break
+    else:
+        print(i)
+
+#while loop
+i=1
+while i<=10:
+    if i==5:
+        pass
+    print(i)
+    i=i+1
