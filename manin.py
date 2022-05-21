086a9db5b8208734e8675df77985c39976a138f9
Teja Narni <75619472+Tejanarni@users.noreply.github.com>
2022-05-21 19:34:22 +0530

    Create manin.py

---
+++ manin.py
@@ -0,0 +1,16 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+n=int(input("Enter n value" ))
+i=1
+while i<=n:
+    print(i,end="")
+    i=i+1
+
+
+
+
+#for loop
+for i in range(5):
+    for j in range(5):
+        print("*",end="")
+    print("")
